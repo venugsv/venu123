863bc163486afbae6ebded4cc9562331347277f5
tm <tm@tm15>
2018-04-18 12:32:29 +0530

    version 4

---
+++ src/venu123/Testgit.java
@@ -0,0 +1,12 @@
+package venu123;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my new project git test");
+		
+	}
+
+}
